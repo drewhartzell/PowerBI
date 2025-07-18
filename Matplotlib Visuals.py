b090e05a44fd2649abdb4149fef245252bb97283
Drew Hartzell <151415194+drewhartzell@users.noreply.github.com>
2025-07-17 22:20:02 -0400

    Delete Matplotlib Visuals.py

--- Matplotlib Visuals.py
+++
@@ -1,9 +0,0 @@
-# Example matplotlib visuals #
-
-import pandas as pd
-import matplotlib.pyplot as plt
-
-df = pd.DataFrame({'X': [1, 2, 3], 'Y': [4, 5, 6]})
-plt.plot(df['X'], df['Y'])
-plt.title("Test Plot")
-plt.show()
